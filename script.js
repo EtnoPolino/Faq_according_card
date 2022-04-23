2ea489aa788d57a9fa796b885c5c280eb0e8c388
EtnoPolino <einshoch66@gmail.com>
2022-04-23 20:48:26 +0200

    update : transition of answer

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const selectQuestions = document.querySelectorAll('.question');
-
+const selectQuestions = document.querySelectorAll(".question");
 
 selectQuestions.forEach((question) => {
-    question.addEventListener('click', () => {
-        question.nextElementSibling.classList.toggle('hide'); 
-        question.children[1].classList.toggle('rotate');
-    });
+  question.addEventListener("click", collapseAnswers);
 });
+
+
+function collapseAnswers(){
+    this.children[1].classList.toggle("rotate");
+
+    const panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+}
